9a856bceb0410dd35fd3211a6a24efa6fe819986
Carolina Herbster <carolhmj@gmail.com>
2016-10-26 12:09:34 -0300

    Começando próximo teste

--- src/interface/main.cpp
+++ src/interface/main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
 #include "beamelement2d.h"
 #include "vectordofload.h"
 #include "model.h"
+#include "projectdefines.h"
 #include <Eigen/Core>
 #include <Eigen/QR>
 #include <iostream>
@@ -70,6 +71,65 @@ int main(int argc, char *argv[])
     FullPivHouseholderQR<MatrixXd> solver(globalMatrix);
     VectorXd displacementVector = solver.solve(forceVector);
     std::cout << "Result: " << endl << displacementVector << endl;
+
+    //Exemplo 2: http://media.cheggcdn.com/media%2F24b%2F24bda2dd-e3be-4fcd-81ae-668ea7deb4fd%2Fimage
+    //A resolução será feita com um modelo com 4 nós e 3 elementos
+    std::vector<VectorDOFType> e2n1types = {VectorDOFType::TRANSLATION, VectorDOFType::TRANSLATION, VectorDOFType::ROTATION};
+    std::vector<RestrictionTypes> e2n1restrictions = {RestrictionTypes::FIXED, RestrictionTypes::FIXED, RestrictionTypes::FREE};
+    std::vector<int> e2n1equations = {-1,-1,0};
+    VectorDOF *e2n1v = new VectorDOF(e2n1types, e2n1restrictions, e2n1equations);
+    Node *e2n1 = new Node(Vector3d(0.0,0.0,0.0), e2n1v);
+
+    std::vector<VectorDOFType> e2n2types = {VectorDOFType::TRANSLATION, VectorDOFType::TRANSLATION, VectorDOFType::ROTATION};
+    std::vector<RestrictionTypes> e2n2restrictions = {RestrictionTypes::FREE, RestrictionTypes::FREE, RestrictionTypes::FREE};
+    std::vector<int> e2n2equations = {1,2,3};
+    VectorDOF *e2n2v = new VectorDOF(e2n2types, e2n2restrictions, e2n2equations);
+    Node *e2n2 = new Node(Vector3d(4.0,0.0,0.0), e2n2v);
+
+    std::vector<VectorDOFType> e2n3types = {VectorDOFType::TRANSLATION, VectorDOFType::TRANSLATION, VectorDOFType::ROTATION};
+    std::vector<RestrictionTypes> e2n3restrictions = {RestrictionTypes::FREE, RestrictionTypes::FIXED, RestrictionTypes::FREE};
+    std::vector<int> e2n3equations = {4,-1,5};
+    VectorDOF *e2n3v = new VectorDOF(e2n3types, e2n3restrictions, e2n3equations);
+    Node *e2n3 = new Node(Vector3d(10.0,0.0,0.0), e2n3v);
+
+    std::vector<VectorDOFType> e2n4types = {VectorDOFType::TRANSLATION, VectorDOFType::TRANSLATION, VectorDOFType::ROTATION};
+    std::vector<RestrictionTypes> e2n4restrictions = {RestrictionTypes::FREE, RestrictionTypes::FREE, RestrictionTypes::FREE};
+    std::vector<int> e2n4equations = {6,7,8};
+    VectorDOF *e2n4v = new VectorDOF(e2n4types, e2n4restrictions, e2n4equations);
+    Node *e2n4 = new Node(Vector3d(13.0,0.0,0.0), e2n4v);
+    vector<Node*> e2nvector = {e2n1, e2n2, e2n3, e2n4};
+
+    //Na questão temos que EI é constante e igual a 1750, logo vou colocar valores fictícios?
+    Section *e2s = new Section(0.0, 1, 1);
+    Material *e2m = new Material(1750, 0.0, 0.0);
+
+    BeamElement2D *e2b1 = new BeamElement2D(e2n1, e2n2, e2s, e2m);
+    BeamElement2D *e2b2 = new BeamElement2D(e2n2, e2n3, e2s, e2m);
+    BeamElement2D *e2b3 = new BeamElement2D(e2n3, e2n4, e2s, e2m);
+    vector<Element*> e2bvector = {e2b1, e2b2, e2b3};
+
+    //Loads
+    VectorXd e2n2lvalues(3);
+    e2n2lvalues << 0,-5,0;
+    VectorDOFLoad *e2n2load = new VectorDOFLoad(e2n2v, e2n2lvalues);
+    VectorXd e2n4lvalues(3);
+    e2n4lvalues << 0,-3,0;
+    VectorDOFLoad *e2n4load = new VectorDOFLoad(e2n4v, e2n4lvalues);
+    vector<Load*> e2lvector = {e2n2load, e2n4load};
+
+    Model *m2 = new Model("Simple Beam 2D Test 2", e2nvector, e2bvector, e2lvector);
+    std::cout << m2->printInfo();
+
+    MatrixXd e2globalMatrix = m2->getGlobalStiffnessMatrix();
+    std::cout << "Global stiffness matrix: " << endl << e2globalMatrix << endl;
+
+    VectorXd e2forceVector = m2->getGlobalForceVector();
+    std::cout << "Global force  vector: " << endl << e2forceVector << endl;
+
+    FullPivHouseholderQR<MatrixXd> e2solver(e2globalMatrix);
+    VectorXd e2displacementVector = solver.solve(e2forceVector);
+    std::cout << "Result: " << endl << e2displacementVector << endl;
+
 
 }
 #endif
